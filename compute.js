8e7a16b7985516e929c80788bf7fb49511ad7e16
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-07-20 20:39:47 -0400

    virtually no compute shader

--- compute.js
+++ compute.js
@@ -7,11 +7,7 @@ ${circleStruct.code}
 @group(0) @binding(0) var<storage, read_write> circlesOld : array<Circle>; 
 
 @compute @workgroup_size(1) fn sortAndDisplay() {
-        // insertionSort(0, arrayLength(&circlesOld));
-        let startIdx = 0u;
-        let endIdx = arrayLength(&circlesOld);
-
-        for(var i = startIdx; i < endIdx-1;) {
+        for(var i = 0u; i < 1;) {
             let cand = circlesOld[i];
             circlesOld[i+1] = cand;
         }
