fb03773da1834b8025e13c370dade51ec17777aa
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-07-20 20:43:02 -0400

    shrinking render

--- render.js
+++ render.js
@@ -2,8 +2,6 @@ import { circleStruct } from "./structs.js";
 export const renderShaderCode = (polysPerCircle) => /* wgsl */ `
 ${circleStruct.code}
 
-const PI = radians(180.0);
-
 struct VertexOutput {
     @builtin(position) position : vec4f,
     @location(0) color : vec4f
@@ -14,15 +12,10 @@ struct VertexOutput {
 @vertex fn triangle(@builtin(vertex_index) vertexIdx : u32, 
                     @builtin(instance_index) instanceIdx : u32) -> VertexOutput {
     let circle = circles[instanceIdx];
-    let r = select(0., circle.radius, (vertexIdx & 1) == 0); // Alternate between edges and center
-    let segments = ${polysPerCircle}.;
-
-    // TODO precompute matrix
-    let angle = 2 * PI * f32(vertexIdx) / (2*segments);
 
     return VertexOutput(
-        vec4f(circle.center + (r * vec2f(cos(angle), sin(angle))), 0, 1.),
-        select(vec4(), circle.color, (vertexIdx & 1) == 0)
+        vec4f(circle.center, 0, 1.),
+        circle.color
     );
 }
 
